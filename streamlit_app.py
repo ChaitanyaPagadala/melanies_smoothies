e8855ec9563fa829047c4d096423a989639f33f9
ChaitanyaPagadala <pchaitanya9988@gmail.com>
2024-05-21 12:41:15 -0500

    Update streamlit_app.py
    Returns json for fruityvice data

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,4 +37,4 @@ if Ingrdients_list:
 #New section to display fruityvice nutrition information
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
